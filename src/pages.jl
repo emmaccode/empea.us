8ac0e9ac3d445fd9531ba8ad02890482b32d1e33
Em <52672675+emmettgb@users.noreply.github.com>
2022-03-21 02:15:07 -0400

    added TL environment

---
+++ src/pages.jl
@@ -0,0 +1,14 @@
+function four04()
+    html("""
+    <center>
+    <image src = "images/cover.png"></image>
+    </br>
+    <h1> It looks like this directory could not be found.</h1>
+    Thank you for visiting here at Empea. Thank you for sharing the world. We are
+    sorry you are lost, or we made a mistake. Would you like to report this issue?
+    </center>
+    """)
+    Button()
+end
+
+end # module
